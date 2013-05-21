bd290b305616915dae44f71dc1df63869522926b
volpino <fox91@anche.no>
2013-05-21 14:25:41 +0200

    First implementation

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+required = [
+    'requests==1.2.0'
+]
+
+setup(
+    name='wikipedia_template_parser',
+    version='0.1',
+    description='Simple library for extracting data from Wikipedia templates',
+    author='Federico Scrinzi',
+    author_email='scrinzi@spaziodati.eu',
+    url='https://github.com/SpazioDati/Wikipedia-Template-Parser',
+    packages= ['wikipedia_template_parser'],
+    install_requires=required,
+    license='MIT',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Internet',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
---
+++ wikipedia_template_parser/__init__.py
@@ -0,0 +1,105 @@
+"""
+Wikipedia-Template-Parser
+A simple library for extracting data from Wikipedia templates
+"""
+
+import re
+import requests
+
+
+def clean_wiki_links(s):
+    """
+    Given a wiki text removes links syntax
+    Examples: [[Page]] -> Page
+              [[Page|displayed text]] -> displayed text
+    """
+    # clean links without renaming
+    s = re.sub(r'\[\[([^\|\]]+)\]\]', r'\1', s)
+    # clean links with renaming
+    s = re.sub(r'\[\[[^\|\]]+\|([^\]]+)\]\]', r'\1', s)
+    return s
+
+
+def get_wikitext_from_api(page, lang='en'):
+    """
+    Given a page title and the language returns the wiki text of the latest
+    revision of the page
+    """
+    url = 'http://{}.wikipedia.org/w/api.php'.format(lang)
+    params = {
+        'action': 'query',
+        'prop': 'revisions',
+        'titles': page,
+        'rvprop': 'content',
+        'rvlimit': '1',
+        'format': 'json',
+    }
+    res = requests.get(url, params=params)
+    if not res.ok:
+        res.raise_for_status()
+    json_pages = res.json()['query']['pages']
+    return json_pages.values()[0]['revisions'][0]['*']
+
+
+def data_from_templates(page, lang='en'):
+    """
+    Given a page title and the language returns a list with the data of every
+    template included in the page.
+    Every list item is a dictionary with 2 keys: name and data. name is the
+    name of the template while data is a dictionary with key/value attributes
+    of the template.
+    """
+    store = []
+    content = ' '.join(get_wikitext_from_api(page, lang).split())
+    match = re.findall(r'\{\{([^}]+)\}\}', content)
+    for template_string in match:
+        anon_counter = 0
+        template_string = clean_wiki_links(template_string)
+        template_string = template_string.split("|")
+        name, key_values = template_string[0], template_string[1:]
+        data = {}
+        for key_value in key_values:
+            try:
+                key, value = key_value.split("=")
+            except ValueError:
+                anon_counter += 1
+                key = 'anon_{}'.format(anon_counter)
+                value = key_value
+            data[key.strip()] = value.strip()
+        store.append({'name': name, 'data': data})
+    return store
+
+
+def pages_with_template(template, lang='en', eicontinue=None):
+    """
+    Returns a list of pages that use the given template
+    """
+    url = 'http://{}.wikipedia.org/w/api.php'.format(lang)
+    params = {
+        'action': 'query',
+        'list': 'embeddedin',
+        'eititle': template,
+        'eilimit': 500,
+        'format': 'json',
+    }
+    if eicontinue:
+        params['eicontinue'] = eicontinue
+    res = requests.get(url, params=params)
+    if not res.ok:
+        res.raise_for_status()
+    result = [x['title'] for x in res.json()['query']['embeddedin']]
+    try:
+        eicontinue = res.json()['query-continue']['embeddedin']['eicontinue']
+    except KeyError:
+        eicontinue = None
+    if eicontinue:
+        result += pages_with_template(template, lang, eicontinue)
+    return result
+
+
+if __name__ == "__main__":
+    print pages_with_template("Template:Edificio_religioso", "it")
+    print
+    print data_from_templates("Volano_(Italia)", "it")
+    print
+    print data_from_templates("Cattedrale di San Vigilio", "it")
